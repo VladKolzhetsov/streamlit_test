2e2f8a61fb5a45dc65b6b153bb87015de70e5fac
VladKolzhetsov <stickycherry0@gmail.com>
2026-08-12 13:40:07 -0500

    Update test.py

--- test.py
+++ test.py
@@ -57,4 +57,4 @@ st.markdown( f":{emotion2color[emotion]}-badge[ {emotion} ] with :{emotion2color
 memory_buffer.push( user_input, emotion, prob )
 
 df_messages = memory_buffer.get_dataframe(color_rows_in_dataframe_func = color_rows_in_dataframe)
-st.data_editor( df_messages, disabled=["Message", "Confidence" "Emotion"] )
+st.data_editor( df_messages, disabled=["Message", "Confidence", "Emotion"] )
