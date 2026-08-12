0ca60e1c74e77cbe4a15f20d75bc79905bf00c7f
VladKolzhetsov <stickycherry0@gmail.com>
2026-08-12 07:42:15 -0500

    Update test.py

--- test.py
+++ test.py
@@ -3,4 +3,8 @@ from transformers import pipeline
 
 pipe = pipeline("text-classification", model="VK26/disilbert-finetuned-emotion2")
 
-st.write(pipe("Fuck you, asshole !!!"))
+st.header("Message")
+user_input = st.text_area("label goes here", "")
+
+st.header("Responce")
+st.write(pipe(user_input))
