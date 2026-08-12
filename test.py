1c696e11268870fe0d9a4c2344600d9beedb0d23
VladKolzhetsov <stickycherry0@gmail.com>
2026-08-12 06:26:38 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,20 +1,3 @@
-"""
-# My first app
-Here's our first attempt at using data to create a table:
-"""
-
-"""
-import streamlit as st
-import pandas as pd
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
-
-df
-"""
-
-
 import streamlit as st
 from transformers import pipeline
 
