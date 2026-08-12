72740c02f9498873219629097243a51c87a66bae
VladKolzhetsov <stickycherry0@gmail.com>
2026-08-12 09:58:56 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,22 @@
 import streamlit as st
 from transformers import pipeline
 
+emotion2color = {
+  "sadness" : "blue",
+  "joy" : "yellow",
+  "neutral": "gray",
+  "anger": "red",
+  "fear": "violet",
+  "surprise": "orange",
+  "disgust": "green"
+}
+
 pipe = pipeline("text-classification", model="VK26/disilbert-finetuned-emotion2")
 
 st.header("Message")
 user_input = st.text_area("label goes here", "")
 
 st.header("Responce")
-st.write(pipe(user_input))
+responce = pipe(user_input)
+emotion, prob = responce.value()
+st.badge(f"{emotion} with {round(prob, 3) * 100} % confidence", color = emotion2color[emotion])
