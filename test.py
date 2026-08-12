034e1434ff3edccf29aba266d6dc956b21d837cd
VladKolzhetsov <stickycherry0@gmail.com>
2026-08-12 13:42:55 -0500

    Update test.py

--- test.py
+++ test.py
@@ -58,3 +58,5 @@ memory_buffer.push( user_input, emotion, prob )
 
 df_messages = memory_buffer.get_dataframe(color_rows_in_dataframe_func = color_rows_in_dataframe)
 st.data_editor( df_messages, disabled=["Message", "Confidence", "Emotion"] )
+
+st.bar_chart(df['Emotion'])
