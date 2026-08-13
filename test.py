bfb05c5cfd0e5300f0a9a16de944fd69b55ec3cd
VladKolzhetsov <stickycherry0@gmail.com>
2026-08-13 01:56:57 -0500

    Update test.py

--- test.py
+++ test.py
@@ -64,30 +64,31 @@ def submit():
 st.text_area("message goes here", "", max_chars=250, placeholder="Nothing", key="message", on_change=submit)
 #user_input = st.session_state.user_input
 
+
+st.header("Responce")
+responce = pipe(st.session_state.user_input)[0]
+emotion, prob = responce.values()
+st.markdown( f":{emotion2color[emotion]}-badge[ {emotion} ] with :{emotion2color[emotion]}-badge[ {round(prob * 100, 3)}% ] confidence." )
 if st.session_state.user_input:
-    st.header("Responce")
-    responce = pipe(st.session_state.user_input)[0]
-    emotion, prob = responce.values()
-    st.markdown( f":{emotion2color[emotion]}-badge[ {emotion} ] with :{emotion2color[emotion]}-badge[ {round(prob * 100, 3)}% ] confidence." )
     memory_buffer.push( st.session_state.user_input, emotion, prob )
-    st.session_state.user_input = ""
+st.session_state.user_input = ""
 
-    df_messages = memory_buffer.get_dataframe(color_rows_in_dataframe_func = color_rows_in_dataframe)
-    if "user_advice" not in st.session_state:
-        st.session_state.user_advice = list()
-    st.data_editor( df_messages, disabled=["Message", "Confidence", "Emotion"], key="user_responce", num_rows="fixed" )
-    st.session_state.user_advice = st.session_state["user_responce"]["edited_rows"]
-    st.write(st.session_state.user_advice)
+df_messages = memory_buffer.get_dataframe(color_rows_in_dataframe_func = color_rows_in_dataframe)
+if "user_advice" not in st.session_state:
+    st.session_state.user_advice = list()
+st.data_editor( df_messages, disabled=["Message", "Confidence", "Emotion"], key="user_responce", num_rows="fixed" )
+st.session_state.user_advice = st.session_state["user_responce"]["edited_rows"]
+st.write(st.session_state.user_advice)
 
 
 
-    cnt_emotions = Counter(df_messages.data["Emotion"].tolist())
-    cnt_emotions = data_for_histogram_counter(cnt_emotions)
-    df_cnt_emotions = pd.DataFrame.from_dict(cnt_emotions, orient='index')
-    df_cnt_emotions = df_cnt_emotions.rename({0 : 'count'}, axis='columns')
-    df_cnt_emotions.reset_index(inplace=True)
-    df_cnt_emotions = df_cnt_emotions.rename(columns = {'index' : 'emotions'})
-    histogram = alt.Chart(df_cnt_emotions).mark_bar().encode(x = 'emotions', y = 'count')
-    st.write(histogram)
+cnt_emotions = Counter(df_messages.data["Emotion"].tolist())
+cnt_emotions = data_for_histogram_counter(cnt_emotions)
+df_cnt_emotions = pd.DataFrame.from_dict(cnt_emotions, orient='index')
+df_cnt_emotions = df_cnt_emotions.rename({0 : 'count'}, axis='columns')
+df_cnt_emotions.reset_index(inplace=True)
+df_cnt_emotions = df_cnt_emotions.rename(columns = {'index' : 'emotions'})
+histogram = alt.Chart(df_cnt_emotions).mark_bar().encode(x = 'emotions', y = 'count')
+st.write(histogram)
 
-    st.write(st.session_state["user_responce"])
+st.write(st.session_state["user_responce"])
