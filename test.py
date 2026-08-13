e3d0197cb76f68e899683012bc0689b414802b89
VladKolzhetsov <stickycherry0@gmail.com>
2026-08-13 06:41:34 -0500

    Update test.py

--- test.py
+++ test.py
@@ -154,7 +154,7 @@ def confusion_matrix_plot(cnt_emo, cnt_adv_emo):
 
     fig, ax = plt.subplots()
     dis = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=lables)
-    st.pyplot(dis)
+    dis.pyplot()
 
 if st.button("Commit changes", on_click=commit_changes):
     seaborn_pairplot(cnt_emo, cnt_adv_emo)
