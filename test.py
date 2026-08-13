d4edcec2c2abc679e20996d111bf38493fe69cbf
VladKolzhetsov <stickycherry0@gmail.com>
2026-08-13 09:33:01 -0500

    Update test.py

--- test.py
+++ test.py
@@ -78,50 +78,6 @@ def start_new_session():
     st.session_state.clear()
     st.rerun()
 
-st.button("Start New Session", type="primary", on_click=start_new_session)
-
-memory_buffer = Memory_buffer()
-
-pipe = pipeline("text-classification", model="VK26/disilbert-finetuned-emotion2")
-
-st.header("Message")
-if "user_input" not in st.session_state:
-    st.session_state.user_input = ""
-
-def submit():
-    st.session_state.user_input = st.session_state.message
-    st.session_state.message = ""
-st.text_area("", "", max_chars=250, placeholder="text goes here", key="message", on_change=submit)
-
-
-st.header("Responce")
-responce = pipe(st.session_state.user_input)[0]
-emotion, prob = responce.values()
-st.markdown( f":{emotion2color[emotion]}-badge[ {emotion} ] with :{emotion2color[emotion]}-badge[ {round(prob * 100, 3)}% ] confidence." )
-if st.session_state.user_input:
-    memory_buffer.update()
-    memory_buffer.push( st.session_state.user_input, emotion, prob )
-st.session_state.user_input = ""
-
-df_messages = memory_buffer.get_dataframe(color_rows_in_dataframe_func = color_rows_in_dataframe)
-
-
-if st.session_state.buffer:
-    st.data_editor( df_messages,
-                   disabled=["Message", "Confidence", "Emotion"],
-                   key="user_responce",
-                   num_rows="fixed",
-                   column_config=column_config )
-else:
-    st.data_editor( df_messages,
-                   disabled=["Message", "Confidence", "Emotion"],
-                   key="user_responce",
-                   num_rows="fixed" )
-
-df_messages.data.loc[df_messages.data["Your advice"] == '', 'Your advice'] = df_messages.data["Emotion"]
-cnt_emo = df_messages.data["Emotion"].tolist()
-cnt_adv_emo = df_messages.data["Your advice"].tolist()
-
 def commit_changes():
     memory_buffer.update()
 
@@ -171,9 +127,56 @@ def confusion_matrix_plot(cnt_emo, cnt_adv_emo):
     plt.tight_layout()
     st.pyplot(fig)
 
-if st.button("Commit changes", on_click=commit_changes):
+def submit():
+    st.session_state.user_input = st.session_state.message
+    st.session_state.message = ""
+
+
+
+if "__main__" == __name__:
+
+    st.button("Start New Session", type="primary", on_click=start_new_session)
+
+    memory_buffer = Memory_buffer()
+
+    pipe = pipeline("text-classification", model="VK26/disilbert-finetuned-emotion2")
+
+    st.header("Message")
+    if "user_input" not in st.session_state:
+        st.session_state.user_input = ""
+    st.text_area("", "", max_chars=250, placeholder="text goes here", key="message", on_change=submit)
+
+
+    st.header("Responce")
+    responce = pipe(st.session_state.user_input)[0]
+    emotion, prob = responce.values()
+    st.markdown( f":{emotion2color[emotion]}-badge[ {emotion} ] with :{emotion2color[emotion]}-badge[ {round(prob * 100, 3)}% ] confidence." )
+    if st.session_state.user_input:
+        memory_buffer.update()
+        memory_buffer.push( st.session_state.user_input, emotion, prob )
+    st.session_state.user_input = ""
+
+    df_messages = memory_buffer.get_dataframe(color_rows_in_dataframe_func = color_rows_in_dataframe)
+
+    if st.session_state.buffer:
+        st.data_editor( df_messages,
+                   disabled=["Message", "Confidence", "Emotion"],
+                   key="user_responce",
+                   num_rows="fixed",
+                   column_config=column_config )
+    else:
+        st.data_editor( df_messages,
+                   disabled=["Message", "Confidence", "Emotion"],
+                   key="user_responce",
+                   num_rows="fixed" )
+
+    df_messages.data.loc[df_messages.data["Your advice"] == '', 'Your advice'] = df_messages.data["Emotion"]
+    cnt_emo = df_messages.data["Emotion"].tolist()
+    cnt_adv_emo = df_messages.data["Your advice"].tolist()
+
+    if st.button("Commit changes", on_click=commit_changes):
+        seaborn_pairplot(cnt_emo, cnt_adv_emo)
+        confusion_matrix_plot(cnt_emo, cnt_adv_emo)
+  
     seaborn_pairplot(cnt_emo, cnt_adv_emo)
     confusion_matrix_plot(cnt_emo, cnt_adv_emo)
-  
-seaborn_pairplot(cnt_emo, cnt_adv_emo)
-confusion_matrix_plot(cnt_emo, cnt_adv_emo)
